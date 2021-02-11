460edde47c64a4988afbd1563eae31b707fd62c0
uniqueann <uniqueann@163.com>
2021-02-11 23:02:24 +0800

    normal

--- Af.Core/Controllers/AuthController.cs
+++ Af.Core/Controllers/AuthController.cs
@@ -5,11 +5,14 @@ using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Af.Core.Common;
+using Af.Core.Common.Converter;
 using Af.Core.Common.Helper;
+using Af.Core.Extensions.Authorizations.Helper;
 using Af.Core.Extensions.Authorizations.Policys;
 using Af.Core.IServices;
 using Af.Core.Model.Models;
 using Af.Core.Model.ViewModels;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -17,7 +20,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Af.Core.Controllers
 {
     [Produces("application/json")]
-    [Route("api/auth/[action]")]
+    [Route("api/auth")]
     [ApiController]
     [AllowAnonymous]
     public class AuthController : ControllerBase
@@ -49,6 +52,7 @@ namespace Af.Core.Controllers
         /// <param name="pass"></param>
         /// <returns></returns>
         [HttpGet]
+        [Route("getJwtTokenStr")]
         public async Task<object> GetJwtTokenStr(string name, string pass)
         {
             string jwtStr = string.Empty;
@@ -88,6 +92,7 @@ namespace Af.Core.Controllers
         /// <param name="pass"></param>
         /// <returns></returns>
         [HttpGet]
+        [Route("getJwtToken")]
         public async Task<MessageModel<TokenInfoViewModel>> GetJwtToken(string name = "", string pass = "")
         {
             var jwtStr = string.Empty;
@@ -119,17 +124,85 @@ namespace Af.Core.Controllers
                 // jwt | ids4
                 if (!Permissions.IsUseIds4)
                 {
-                    //var data = await _roleServices
+                    var data = await _roleModulePermissionServices.RoleModuleMaps();
+                    var list = (from item in data
+                                where item.IsDeleted == false
+                                orderby item.Id
+                                select new PermissionItem
+                                {
+                                    Url = item.Module?.LinkUrl,
+                                    Role = item.Role?.Name.ObjToString()
+                                }).ToList();
+                    _permissionRequirement.Permissions = list;
+                }
+
+                var token = JwtToken.BuildJwtToken(claims.ToArray(), _permissionRequirement);
+                return new MessageModel<TokenInfoViewModel> {
+                    success = true,
+                    msg = "获取成功",
+                    response = token
+                };
+            }
+            else
+            {
+                return new MessageModel<TokenInfoViewModel>
+                {
+                    success = false,
+                    msg = "认证失败"
+                };
+            }
+
+        }
+
+        /// <summary>
+        /// 刷新token
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("refreshToken")]
+        public async Task<MessageModel<TokenInfoViewModel>> RefreshToken(string token = "")
+        {
+            var jwtStr = string.Empty;
+            if (string.IsNullOrEmpty(token))
+            {
+                return new MessageModel<TokenInfoViewModel> {
+                    success = false,
+                    msg = "token无效，请重新登录"
+                };
+            }
+
+            var tokenModel = JwtHelper.SerializeJWT(token);
+            if (tokenModel!=null && tokenModel.Uid>0)
+            {
+                var user = await _sysUserInfoServices.QueryById(tokenModel.Uid);
+                if (user!=null)
+                {
+                    var userRoles = await _sysUserInfoServices.GetUserRoleNameStr(user.LoginName, user.LoginPwd);
+                    var claims = new List<Claim> {
+                        new Claim(ClaimTypes.Name,user.LoginName),
+                        new Claim(JwtRegisteredClaimNames.Jti, tokenModel.Uid.ToString()),
+                        new Claim(ClaimTypes.Expiration, DateTime.Now.AddSeconds(_permissionRequirement.Expiration.TotalSeconds).ToString())
+                    };
+                    claims.AddRange(userRoles.Split(',').Select(a=>new Claim(ClaimTypes.Role,a)));
+
+                    // 用户标识
+                    var identity = new ClaimsIdentity(JwtBearerDefaults.AuthenticationScheme);
+                    identity.AddClaims(claims);
+
+                    var refreshToken = JwtToken.BuildJwtToken(claims.ToArray(), _permissionRequirement);
+                    return new MessageModel<TokenInfoViewModel> {
+                        success = true,
+                        msg = "获取成功",
+                        response = refreshToken
+                    };
                 }
             }
 
-
-            return new MessageModel<TokenInfoViewModel>
-            {
+            return new MessageModel<TokenInfoViewModel> {
                 success = false,
                 msg = "认证失败"
             };
         }
-
     }
 }
